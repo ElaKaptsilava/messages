373b88c9e520003a561adf3401a6b8e987e06e89
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2023-10-05 18:59:24 +0200

    create docker-compose.yml

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#+=d#&u!c!*9f&&329^+uhjx6%)u^hypvg*$))-8*dsllwbc8%'
+SECRET_KEY = os.environ.get(
+'django-insecure-#+=d#&u!c!*9f&&329^+uhjx6%)u^hypvg*$))-8*dsllwbc8%')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
