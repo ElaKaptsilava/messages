93e8e44afbb340256d8496551f6aaf4b938e1df1
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2023-10-05 19:48:21 +0200

    create new models: mailbox, email, templates

--- backend/assignments/models.py
+++ backend/assignments/models.py
@@ -1,11 +1,40 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
+from django.utils import timezone
 
 
-class Assignment(models.Model):
-    first_term = models.DecimalField(
-        max_digits=5, decimal_places=2, null=False, blank=False
-    )
-    second_term = models.DecimalField(
-        max_digits=5, decimal_places=2, null=False, blank=False
-    )
-    sum = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+class Mailbox(models.Model):
+    host = models.CharField(max_length=300)
+    port = models.IntegerField(default=465)
+    login = models.CharField(max_length=70)
+    password = models.CharField(max_length=20)
+    email_form = models.CharField(max_length=70)
+    use_ssl = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
+    date = models.DateTimeField(default=timezone.now)
+    last_update = models.DateTimeField(default=timezone.now)
+
+    @property
+    def sent(self):
+        return
+
+class Template(models.Model):
+    subject = models.CharField(max_length=50)
+    text = models.TextField()
+    attachment = models.FileField(null=True, blank=True)
+    date = models.DateTimeField(default=timezone.now)
+    last_update = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.subject
+
+
+class Email(models.Model):
+    mailbox = models.ForeignKey(Mailbox, on_delete=models.CASCADE)
+    template = models.ForeignKey(Template, on_delete=models.CASCADE)
+    to = ArrayField(models.EmailField(max_length=70), size=5, default=list)
+    cc = ArrayField(models.EmailField(max_length=70), default=list, blank=True)
+    bcc = ArrayField(models.EmailField(max_length=70), default=list, blank=True)
+    reply_to = models.EmailField(default=None, max_length=70, blank=True)
+    sent_date = models.DateTimeField(blank=True, null=True)
+    date = models.DateTimeField(default=timezone.now)
