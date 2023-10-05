97ebf4095ab6091fe7757ebdba0c3ffc3009b70c
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2023-10-05 19:54:46 +0200

    create sending_email func. in task.py

--- backend/assignments/tasks.py
+++ backend/assignments/tasks.py
@@ -1,13 +1,31 @@
+from .models import *
 from celery import shared_task
-
-from .models import Assignment
+from django.core.mail import send_mail
+from backend import settings
 
 
 @shared_task()
-def task_execute(job_params):
+def sending_email(params):
+    email = Email.objects.get(id=params['db_id'])
 
-    assignment = Assignment.objects.get(pk=job_params["db_id"])
+    if len(email.to) > 0:
+        send_mail(
+            subject=email.template.subject,
+            message=email.template.text,
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=email.to + email.cc,
+            fail_silently=True
+        )
+        print('Send copy to bcc')
 
-    assignment.sum = assignment.first_term + assignment.second_term
-
-    assignment.save()
+        if len(email.bcc) > 0:
+            send_mail(
+                subject=email.template.subject,
+                message=email.template.text.join('\n There is no need to reply to the message.'),
+                from_email=settings.EMAIL_HOST_USER,
+                recipient_list=email.bcc,
+                fail_silently=True
+            )
+            print('Send copy to bcc')
+    email.sent_date = timezone.now()
+    email.save()
