3b471f2e3f14f4883cf0364dd9380c64a1c9b602
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2023-10-05 17:19:41 +0200

    /backend/urls.py add assignments_urlpatterns

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from assignments.urls import assignments_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+urlpatterns += assignments_urlpatterns
