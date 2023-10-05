acf10b4b09126bedfc167274f71bf11b81a73163
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2023-10-05 17:13:54 +0200

    create assignmentViewSet

--- backend/assignments/views.py
+++ backend/assignments/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.db import transaction
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.exceptions import APIException
+
+from .models import Assignment
+from .serializers import AssignmentSerializer
+from .tasks import task_execute
+
+
+class AssignmentViewSet(viewsets.ModelViewSet):
+
+    serializer_class = AssignmentSerializer
+    queryset = Assignment.objects.all()
+
+    def perform_create(self, serializer):
+        try:
+            with transaction.atomic():
+                instance = serializer.save()
+                instance.save()
+
+                job_params = {"db_id": instance.id}
+
+                transaction.on_commit(lambda: task_execute.delay(job_params))
+
+        except Exception as e:
+            raise APIException(str(e))
