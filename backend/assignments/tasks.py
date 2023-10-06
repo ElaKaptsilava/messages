0d8199b6cd5e162755631b221ffc5e3ef233eebd
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2023-10-06 12:11:37 +0200

    add conditions assignments/tasks.sending_email

--- backend/assignments/tasks.py
+++ backend/assignments/tasks.py
@@ -8,10 +8,13 @@ from .models import Email
 @shared_task()
 def sending_email(params):
     email = Email.objects.get(id=params['db_id'])
-    emails = (
+    emails = [
         (email.template.subject, email.template.text, email.reply_to, email.to),
-        (email.template.subject, email.template.text.join('\nCopy'), email.reply_to, email.cc),
-    )
+    ]
+    if len(email.cc) > 0:
+        email.appeand((email.template.subject, email.template.text.join('\nCopy'), email.reply_to, email.cc))
+    if len(email.bcc) > 0:
+        email.appeand((email.template.subject, email.template.text.join("\nThis is copy.\nDon't answer"), email.reply_to, email.cc))
     result = mail.send_mass_mail(emails)
     email.sent_date = timezone.now()
     email.save()
